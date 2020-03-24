bb50fee773519914e4694c9716d26571865b4ee7
lokko <lokkomokko1@gmail.com>
2020-03-24 20:06:25 +0300

    Updates

---
+++ precache-manifest.d37e912bb11ef9032651156b78764dc3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86e00965dc9e8e9ff37a299baaa547dd",
+    "url": "/vk-glavclub-app/index.html"
+  },
+  {
+    "revision": "edd139ee99038ccc7b97",
+    "url": "/vk-glavclub-app/static/css/2.2cfa1b23.chunk.css"
+  },
+  {
+    "revision": "fdd9203c962d9387a930",
+    "url": "/vk-glavclub-app/static/css/main.b61d0c62.chunk.css"
+  },
+  {
+    "revision": "edd139ee99038ccc7b97",
+    "url": "/vk-glavclub-app/static/js/2.bad90d20.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vk-glavclub-app/static/js/2.bad90d20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdd9203c962d9387a930",
+    "url": "/vk-glavclub-app/static/js/main.9a326c87.chunk.js"
+  },
+  {
+    "revision": "8a6a95b2e4f89b375c51",
+    "url": "/vk-glavclub-app/static/js/runtime-main.1795e8d3.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/vk-glavclub-app/precache-manifest.c4037864994a5a1993ca907defd36fa4.js"
+  "/vk-glavclub-app/precache-manifest.d37e912bb11ef9032651156b78764dc3.js"
 );
 
 self.addEventListener('message', (event) => {
